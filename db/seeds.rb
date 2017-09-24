665ce77e575800f7762c6c161ab46914f25553e7
Steve Brewer <steve.brewer@futurelearn.com>
2017-09-24 08:44:45 +0000

    Create users model, view and controller

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+User.create(first_name: 'Beyonce', last_name: 'Knowles')
+User.create(first_name: 'Lana', last_name: 'Del Rey')
+User.create(first_name: 'Frank', last_name: 'Sinatra')
